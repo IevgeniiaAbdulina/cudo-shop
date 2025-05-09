f955227f057daa4ba7c48e15e35726bcff051555
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2025-05-09 20:58:55 +0200

    feat: Add additional ESLint rules and configuration.

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,7 @@ export default tseslint.config(
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
+      eslintPluginPrettierRecommended
     ],
     languageOptions: {
       ecmaVersion: 2022,
@@ -28,10 +29,12 @@ export default tseslint.config(
       parserOptions: {
         project: ["./tsconfig.json"],
         sourceType: "module",
+        ecmaVersion: 2020,
       },
     },
     processor: angular.processInlineTemplates,
     rules: {
+      "@angular-eslint/directive-class-suffix": "error",
       "@angular-eslint/directive-selector": [
         "error",
         {
@@ -48,20 +51,69 @@ export default tseslint.config(
           style: "kebab-case",
         },
       ],
-      "@typescript-eslint/no-unused-vars": ["error"],
+      "@angular-eslint/component-class-suffix": [
+        "error",
+        {
+          suffixes: ["Component", "Page", "View"],
+        }
+      ],
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        {
+          accessibility: "explicit",
+          overrides: {
+            constructors: "no-public"
+          }
+        }
+      ],
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        }
+      ],
       "@typescript-eslint/no-explicit-any": "error",
-      "prettier/prettier": ["error", {}, { "usePrettierrc": true }]
+      "prettier/prettier": ["error", {}, { "usePrettierrc": true }],
+      "quotes": [
+        "error",
+        "double",
+        { avoidEscape: true }
+      ],
+      "semi": ["error", "always"],
+      "no-console": "off",
+      curly: "error",
+      eqeqeq: ["error", "always"],
+      "prefer-const": "error",
+      "arrow-body-style": ["error", "as-needed"],
+      "no-multiple-empty-lines": ["error", { max: 1 }],
+      "padding-line-between-statements": [
+        "error",
+        { blankLine: "always", prev: "*", next: "return" }
+      ]
     },
+    settings: {
+      "import/resolver": {
+        typescript: {}
+      }
+    }
   },
   {
     files: ["**/*.html"],
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
+      eslintPluginPrettierRecommended
     ],
     rules: {
+      "@angular-eslint/template/banana-in-box": "error",
+      "@angular-eslint/template/no-negated-async": "error",
+      "@angular-eslint/template/cyclomatic-complexity": [
+        "error", {
+        maxComplexity: 5
+      }],
       "prettier/prettier": "error",
     },
   },
-  eslintPluginPrettierRecommended
 );
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'cudo-shop' title`, () => {
+  it("should have the 'cudo-shop' title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual("cudo-shop");
