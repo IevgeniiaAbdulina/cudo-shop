049ca6735a4321b4f4ada95bb3804f29f4caa998
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2025-05-09 19:36:13 +0200

    feat: Install and configure Jest.

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+export default {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!flat)/',
+  ],
+};
+
