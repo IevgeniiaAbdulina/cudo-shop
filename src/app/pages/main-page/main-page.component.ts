442ea52c4f456539ff113d756d48cd15bbfe4b00
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2025-05-15 17:09:09 +0200

    feat: Add navigation to the children components from `Main Page`. > 1. Navigate to Home page content. > 2. Navigate to Books catalog content. > 3. Navigate to Cosmetics catalog content. > 4. Add Service to navigate to specific route.

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,23 +1,33 @@
-import { Component, signal } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
+import { Component, inject, OnInit, signal } from "@angular/core";
+import { ActivatedRoute, Router, RouterOutlet } from "@angular/router";
 import { Book } from "../../shared/interfaces/book";
 import { Cosmetics } from "../../shared/interfaces/cosmetics";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { NavigateToSpecificRouteService } from "../../shared/services/navigate-to-specific-route/navigate-to-specific-route.service";
 
 @Component({
   selector: "app-main-page",
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet],
   templateUrl: "./main-page.component.html",
   styleUrl: "./main-page.component.scss",
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+  private navigateToSpecificRouteService = inject(NavigateToSpecificRouteService);
+
   public title = "cudo-shop";
 
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-  ) {}
+  public ngOnInit() {
+    this.navigateToSpecificRouteService.routeName$.subscribe((data) => {
+      this.navigateToRoute(data);
+    });
+  }
+
+  public navigateToRoute(route: string) {
+    this.router.navigate([route], { relativeTo: this.route });
+  }
 
   public goToSignInPage() {
     this.router.navigate(["/sign-in"], { relativeTo: this.route });
