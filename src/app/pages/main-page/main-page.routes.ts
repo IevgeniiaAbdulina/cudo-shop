e201908ddbe211d7511321091960bc91c91bed7e
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2025-05-15 17:12:04 +0200

    feat: Add Routing. > 1. Add Application Routing. > 2. Add Main Page Routing. > 3. Add Lazy Loading for Main Page loading.

---
+++ src/app/pages/main-page/main-page.routes.ts
@@ -0,0 +1,32 @@
+import { Routes } from "@angular/router";
+import { MainPageComponent } from "./main-page.component";
+import { HomeComponent } from "./components/home/home.component";
+import { CatalogBooksComponent } from "./components/catalog-books/catalog-books.component";
+import { CatalogCosmeticsComponent } from "./components/catalog-cosmetics/catalog-cosmetics.component";
+
+export const mainRoutes: Routes = [
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full",
+  },
+  {
+    path: "",
+    title: "Cudo Shop",
+    component: MainPageComponent,
+    children: [
+      {
+        path: "home",
+        component: HomeComponent,
+      },
+      {
+        path: "books",
+        component: CatalogBooksComponent,
+      },
+      {
+        path: "cosmetics",
+        component: CatalogCosmeticsComponent,
+      },
+    ],
+  },
+];
