c236c620276821e1c74179a2486d8e93a645e1ab
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2025-05-15 17:12:04 +0200

    feat: Add Routing. > 1. Add Application Routing. > 2. Add Main Page Routing. > 3. Add Lazy Loading for Main Page loading.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from "@angular/router";
+import { SignInPageComponent } from "./pages/sign-in-page/sign-in-page.component";
+import { SignUpPageComponent } from "./pages/sign-up-page/sign-up-page.component";
+import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import("./pages/main-page/main-page.routes").then((m) => m.mainRoutes),
+  },
+  {
+    path: "sign-in",
+    component: SignInPageComponent,
+  },
+  {
+    path: "sign-up",
+    component: SignUpPageComponent,
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent,
+  },
+];
