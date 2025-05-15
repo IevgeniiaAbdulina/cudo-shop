45af9a4b746727bd8fe7b66831b8d0197c4fc49f
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2025-05-15 17:14:47 +0200

    feat: Add Books Catalog component. > 1. Add template for book list. > 2. Transmit book list from main page.

---
+++ src/app/pages/main-page/components/catalog-books/catalog-books.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { CatalogBooksComponent } from "./catalog-books.component";
+
+describe("CatalogBooksComponent", () => {
+  let component: CatalogBooksComponent;
+  let fixture: ComponentFixture<CatalogBooksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CatalogBooksComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CatalogBooksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/main-page/components/catalog-books/catalog-books.component.ts
@@ -0,0 +1,19 @@
+import { Component, inject, Signal } from "@angular/core";
+import { ROUTER_OUTLET_DATA } from "@angular/router";
+import { Book } from "../../../../shared/interfaces/book";
+import { Cosmetics } from "../../../../shared/interfaces/cosmetics";
+
+@Component({
+  selector: "app-catalog-books",
+  imports: [],
+  templateUrl: "./catalog-books.component.html",
+  styleUrl: "./catalog-books.component.scss",
+})
+export class CatalogBooksComponent {
+  private data = inject(ROUTER_OUTLET_DATA) as Signal<{
+    books: Book[];
+    cosmetics: Cosmetics[];
+  }>;
+
+  public books = this.data().books;
+}
