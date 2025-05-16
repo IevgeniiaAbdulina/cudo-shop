965c7a67a070612b33128498db9d33a6a15c5314
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2025-05-16 13:54:34 +0200

    feat: Add simple style to MainPage. > 1. Add button `Go back` for Books Catalog component. > 2. Add button `Go back` for Cosmetics Catalog component. > refactor: Increase `printWidth` for better code readability. > 1. Apply Prettier formating.

--- src/app/pages/main-page/components/catalog-books/catalog-books.component.ts
+++ src/app/pages/main-page/components/catalog-books/catalog-books.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, Signal } from '@angular/core';
 import { ROUTER_OUTLET_DATA } from '@angular/router';
 import { Book } from '../../../../shared/interfaces/book';
 import { Cosmetics } from '../../../../shared/interfaces/cosmetics';
+import { NavigateToSpecificRouteService } from '../../../../shared/services/navigate-to-specific-route/navigate-to-specific-route.service';
 
 @Component({
   selector: 'app-catalog-books',
@@ -10,10 +11,16 @@ import { Cosmetics } from '../../../../shared/interfaces/cosmetics';
   styleUrl: './catalog-books.component.scss',
 })
 export class CatalogBooksComponent {
+  private navigateToSpecificRouteService = inject(NavigateToSpecificRouteService);
+
   private data = inject(ROUTER_OUTLET_DATA) as Signal<{
     books: Book[];
     cosmetics: Cosmetics[];
   }>;
 
   public books = this.data().books;
+
+  public goBack(): void {
+    this.navigateToSpecificRouteService.setRoute('.');
+  }
 }
