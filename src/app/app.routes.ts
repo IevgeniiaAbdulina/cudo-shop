f1072dd6422de3f5ae8f1a94924ea3e5228db31d
AlenaVP Legion <poprugo@gmail.com>
2025-05-20 16:39:31 +0200

    feat: add Login component with form validation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { RegistrationComponent } from './features/auth/registration/registration.component';
-import { LoginPageComponent } from './features/auth/login-page/login-page.component';
+import { LoginComponent } from './features/auth/login/login.component';
 
 export const routes: Routes = [
   {
@@ -12,11 +12,11 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./features/main-page/main-page.routes').then((m) => m.mainRoutes),
   },
-  // {
-  //   path: 'login',
-  //   pathMatch: 'full',
-  //   component: LoginPageComponent,
-  // },
+  {
+    path: 'login',
+    pathMatch: 'full',
+    component: LoginComponent,
+  },
   {
     path: 'registration',
     pathMatch: 'full',
---
+++ src/app/features/auth/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import ERROR_MSG from '../../../shared/constants/error-message';
+import { EMAIL_REGEX, PASSWORD_REGEX } from '../../../shared/constants/regex';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent implements OnInit {
+  public errMsg = ERROR_MSG;
+  public loginForm!: FormGroup;
+  public passwordFieldType: string = 'password';
+  public isPasswordVisible: boolean = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern(EMAIL_REGEX)]],
+      password: ['', [Validators.required, Validators.pattern(PASSWORD_REGEX)]],
+    });
+  }
+
+  public onSubmit(): void {
+    if (this.loginForm.valid) {
+      // Handle form submission
+      console.log(this.loginForm.value);
+    }
+  }
+
+  public getFieldError(fieldName: string) {
+    let error = '';
+    let field = this.loginForm.get(fieldName);
+    if (!field) {
+      field = this.loginForm.controls['address'].get(fieldName);
+    }
+    if (field?.errors) {
+      error = Object.keys(field?.errors)[0];
+    }
+
+    return error;
+  }
+
+  public togglePasswordVisibility(): void {
+    this.isPasswordVisible = !this.isPasswordVisible;
+    this.passwordFieldType = this.isPasswordVisible ? 'text' : 'password';
+  }
+}
