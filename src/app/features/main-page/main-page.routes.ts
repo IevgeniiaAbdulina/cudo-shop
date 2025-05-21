529d67cf8cd7d1c2eafd47afb4ee910941897883
Olga Rekun <rekun.olya@gmail.com>
2025-05-21 21:57:51 +0200

    fix: fix styles for login page

--- src/app/features/main-page/main-page.routes.ts
+++ src/app/features/main-page/main-page.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegistrationComponent } from '../auth/registration/registration.component';
+import { LoginComponent } from '../auth/login/login.component';
 
 export const mainRoutes: Routes = [
   {
@@ -16,6 +17,10 @@ export const mainRoutes: Routes = [
       {
         path: 'registration',
         component: RegistrationComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
       },
       {
         path: 'books',
