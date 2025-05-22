70b33a46c9bb8d17388824bc40d2e91a6ff50566
Olga Rekun <rekun.olya@gmail.com>
2025-05-22 18:24:29 +0200

    feat: add style to home page

--- src/app/features/main-page/components/home/home.component.ts
+++ src/app/features/main-page/components/home/home.component.ts
@@ -3,10 +3,10 @@ import { ROUTER_OUTLET_DATA } from '@angular/router';
 import { Book } from '../../../../shared/interfaces/book';
 import { Cosmetics } from '../../../../shared/interfaces/cosmetics';
 import { NavigateToSpecificRouteService } from '../../../../shared/services/navigate-to-specific-route/navigate-to-specific-route.service';
-
+import { ButtonComponent } from '../../../../shared/ui/button/button.component';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
