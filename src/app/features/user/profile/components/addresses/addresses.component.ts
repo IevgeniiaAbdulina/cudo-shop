cd3055fd92a1ccca9cec2526181fee0de10f5bd2
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2025-05-31 18:43:45 +0200

    feat: Add User Info Layout and Highlighting Default Addresses. > 1. Divide the User Profile page into two sections. > 2. Display personal information section. > 3. Display list ser addresses section. > 4. Add routing for User Profile child components. > 5. Add default shipping and billing addresses style.

---
+++ src/app/features/user/profile/components/addresses/addresses.component.ts
@@ -0,0 +1,86 @@
+import { Component, signal, WritableSignal, OnInit } from '@angular/core';
+import { ButtonComponent } from '../../../../../shared/ui/button/button.component';
+import { FormatCountryPipe } from '../../../pipes/format-country.pipe';
+import { TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { UserModel } from '../../../model/user-model';
+import { Address, UserResponse } from '../../../interfaces/user-response';
+import { UserService } from '../../../services/user.service';
+
+@Component({
+  selector: 'app-addresses',
+  imports: [ButtonComponent, FormatCountryPipe, TitleCasePipe, UpperCasePipe],
+  templateUrl: './addresses.component.html',
+  styleUrl: './addresses.component.scss',
+})
+export class AddressesComponent implements OnInit {
+  public user: WritableSignal<UserModel | null> = signal<UserModel | null>(null);
+
+  public defaultBillingAddress?: Address;
+  public defaultShippingAddress?: Address;
+  public billingAddresses?: Address[];
+  public shippingAddresses?: Address[];
+
+  constructor(private userService: UserService) {}
+
+  public ngOnInit(): void {
+    this.userService.getUserPersonalInfoByToken().subscribe({
+      next: (response: UserResponse) => {
+        console.log('[user profile page] response data: ', response);
+
+        this.user.set(
+          new UserModel(
+            response.id,
+            response.email,
+            response.firstName,
+            response.lastName,
+            response.dateOfBirth,
+            response.password,
+            response.addresses,
+          ),
+        );
+
+        this.defaultBillingAddress = this.findDefaultBillingAddress(response);
+        this.defaultShippingAddress = this.findDefaultShippingAddress(response);
+        this.billingAddresses = this.findBillingAddresses(response);
+        this.shippingAddresses = this.findShippingAddresses(response);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  public findDefaultBillingAddress(response: UserResponse): Address | undefined {
+    return this.user()?.addresses.find((address: Address) => response.defaultBillingAddressId === address.id);
+  }
+
+  public findDefaultShippingAddress(response: UserResponse): Address | undefined {
+    return this.user()?.addresses.find((address: Address) => response.defaultShippingAddressId === address.id);
+  }
+
+  public findBillingAddresses(response: UserResponse): Address[] | undefined {
+    const addresses: Address[] = [];
+    response.billingAddressIds.forEach((addressId: string) => {
+      this.user()?.addresses.forEach((address: Address) => {
+        if (address.id === addressId) {
+          addresses?.push(address);
+        }
+      });
+    });
+
+    return addresses;
+  }
+
+  public findShippingAddresses(response: UserResponse): Address[] | undefined {
+    const addresses: Address[] = [];
+    response.shippingAddressIds.forEach((addressId: string) => {
+      this.user()?.addresses.forEach((address: Address) => {
+        if (address.id === addressId) {
+          addresses?.push(address);
+        }
+      });
+    });
+
+    return addresses;
+  }
+}
--- src/app/features/user/profile/profile.component.ts
+++ src/app/features/user/profile/profile.component.ts
@@ -1,88 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../services/user.service';
-import { UserModel } from '../model/user-model';
-import { Address, UserResponse } from '../interfaces/user-response';
-import { DatePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
-  imports: [UpperCasePipe, TitleCasePipe, DatePipe],
+  imports: [TitleCasePipe, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent implements OnInit {
-  public user?: UserModel;
-  public defaultBillingAddress?: Address;
-  public defaultShippingAddress?: Address;
-  public billingAddresses?: Address[];
-  public shippingAddresses?: Address[];
-
-  constructor(private userService: UserService) {}
-
-  public ngOnInit(): void {
-    this.userService.getUserPersonalInfoByToken().subscribe({
-      next: (response: UserResponse) => {
-        console.log('[user profile page] response data: ', response);
-
-        this.user = new UserModel(
-          response.id,
-          response.email,
-          response.firstName,
-          response.lastName,
-          response.dateOfBirth,
-          response.password,
-          response.addresses,
-        );
-
-        this.getAddresses();
-        this.defaultBillingAddress = this.findDefaultBillingAddress(response);
-        this.defaultShippingAddress = this.findDefaultShippingAddress(response);
-        this.billingAddresses = this.findBillingAddresses(response);
-        this.shippingAddresses = this.findShippingAddresses(response);
-      },
-      error: (error) => {
-        console.error(error);
-      },
-    });
-  }
-
-  public getAddresses(): void {
-    this.user?.addresses.map((address: Address) => {
-      console.log('[user profile page] addresses: ', address);
-    });
-  }
-
-  public findDefaultBillingAddress(response: UserResponse): Address | undefined {
-    return this.user?.addresses.find((address: Address) => response.defaultBillingAddressId === address.id);
-  }
-
-  public findDefaultShippingAddress(response: UserResponse): Address | undefined {
-    return this.user?.addresses.find((address: Address) => response.defaultShippingAddressId === address.id);
-  }
-
-  public findBillingAddresses(response: UserResponse): Address[] | undefined {
-    const addresses: Address[] = [];
-    response.billingAddressIds.forEach((addressId: string) => {
-      this.user?.addresses.forEach((address: Address) => {
-        if (address.id === addressId) {
-          addresses?.push(address);
-        }
-      });
-    });
-
-    return addresses;
-  }
-
-  public findShippingAddresses(response: UserResponse): Address[] | undefined {
-    const addresses: Address[] = [];
-    response.shippingAddressIds.forEach((addressId: string) => {
-      this.user?.addresses.forEach((address: Address) => {
-        if (address.id === addressId) {
-          addresses?.push(address);
-        }
-      });
-    });
-
-    return addresses;
-  }
+export class ProfileComponent {
+  public navAccount = 'Personal info';
+  public navAddresses = 'Addresses';
 }
