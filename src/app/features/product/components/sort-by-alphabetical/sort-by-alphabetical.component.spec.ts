9afd1a2dd298fb140842692860db824ff9b27482
Olga Rekun <rekun.olya@gmail.com>
2025-06-09 23:32:02 +0200

    fix: fix No provider for _HttpClient! test for sort-by-alphabetical.component.spec.ts

--- src/app/features/product/components/sort-by-alphabetical/sort-by-alphabetical.component.spec.ts
+++ src/app/features/product/components/sort-by-alphabetical/sort-by-alphabetical.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 import { SortByAlphabeticalComponent } from './sort-by-alphabetical.component';
 
@@ -9,6 +10,7 @@ describe('SortByAlphabeticalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SortByAlphabeticalComponent],
+      providers: [provideHttpClient()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SortByAlphabeticalComponent);
