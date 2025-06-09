7bb17d540a675b8bf713e40ab0534874b0f1b063
Olga Rekun <rekun.olya@gmail.com>
2025-06-10 00:32:52 +0200

    fix: fix test for user-model.spec.ts

--- src/app/features/user/model/user-model.spec.ts
+++ src/app/features/user/model/user-model.spec.ts
@@ -2,6 +2,6 @@ import { UserModel } from './user-model';
 
 describe('UserModel', () => {
   it('should create an instance', () => {
-    expect(new UserModel('id', 'email', 'firstName', 'lastName', 'dateOfBirth', 'password', [])).toBeTruthy();
+    expect(new UserModel(0, 'id', 'email', 'firstName', 'lastName', 'dateOfBirth', 'password', [])).toBeTruthy();
   });
 });
