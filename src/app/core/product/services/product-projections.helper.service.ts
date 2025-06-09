58e2cd728c3b7807d026f335dc1108b3da1fbad4
Olga Rekun <rekun.olya@gmail.com>
2025-06-10 01:49:01 +0200

    fix: fix test for product-projections.helper.service.ts

--- src/app/core/product/services/product-projections.helper.service.ts
+++ src/app/core/product/services/product-projections.helper.service.ts
@@ -13,39 +13,39 @@ export class ProductProjectionsHelperService {
   constructor() {}
 
   public getProductName(productProjection: ProductProjection): string {
-    const currentName = productProjection.name;
+    const currentName = productProjection?.name;
 
-    return currentName[getLangKey()];
+    return currentName?.[getLangKey()];
   }
 
   public getProductImg(productProjection: ProductProjection): string {
-    const currentMasterVariantImage: ProductImage = productProjection.masterVariant.images[this.getImgIdx()];
+    const currentMasterVariantImage: ProductImage = productProjection?.masterVariant.images[this.getImgIdx()];
 
-    return currentMasterVariantImage.url;
+    return currentMasterVariantImage?.url;
   }
 
   public getShortProductDescription(productProjection: ProductProjection): string {
-    const currentMetaDescription = productProjection.metaDescription;
-    const description = currentMetaDescription[getLangKey()];
+    const currentMetaDescription = productProjection?.metaDescription;
+    const description = currentMetaDescription?.[getLangKey()];
 
-    return description.length > 89 ? `${description.slice(0, 80)}...` : description;
+    return description?.length > 89 ? `${description.slice(0, 80)}...` : description;
   }
 
   public getProductCurrency(productProjection: ProductProjection): string {
-    const productCurrency = productProjection.masterVariant.prices[this.getPriceIdx()].value.currencyCode;
+    const productCurrency = productProjection?.masterVariant.prices[this.getPriceIdx()].value.currencyCode;
 
     return productCurrency;
   }
 
   public getProductPrice(productProjection: ProductProjection): string {
-    const price = productProjection.masterVariant.prices[this.getPriceIdx()].value.centAmount;
+    const price = productProjection?.masterVariant.prices[this.getPriceIdx()].value.centAmount;
     const productPrice = (price / 100).toFixed(2);
 
     return productPrice;
   }
 
   public hasDiscount(productProjection: ProductProjection): boolean {
-    const isSale = !!productProjection.masterVariant.prices[this.getPriceIdx()].discounted;
+    const isSale = !!productProjection?.masterVariant.prices[this.getPriceIdx()].discounted;
 
     return isSale;
   }
