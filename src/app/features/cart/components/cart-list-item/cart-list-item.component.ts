fa4400d0fa2dd164842d8b293893d2d7ee1549af
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2025-06-14 21:40:44 +0200

    feat: Show Order Summary Data on the UI.

--- src/app/features/cart/components/cart-list-item/cart-list-item.component.ts
+++ src/app/features/cart/components/cart-list-item/cart-list-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, input, InputSignal, OnInit, signal, WritableSignal } from '@angular/core';
 import { ButtonComponent } from '../../../../shared/ui/button/button.component';
-import { CurrencyPipe, DecimalPipe, NgOptimizedImage } from '@angular/common';
+import { DecimalPipe, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { CartItemResponse } from '../../interfaces/cart-response';
 import { CartItemModel } from '../../model/cart-item-model';
@@ -8,7 +8,7 @@ import { discountValue } from '../../../../shared/utils/discount-value';
 
 @Component({
   selector: 'app-cart-list-item',
-  imports: [ButtonComponent, NgOptimizedImage, RouterLink, CurrencyPipe, DecimalPipe],
+  imports: [ButtonComponent, NgOptimizedImage, RouterLink, DecimalPipe],
   templateUrl: './cart-list-item.component.html',
   styleUrl: './cart-list-item.component.scss',
 })
--- src/app/features/cart/model/cart-model.ts
+++ src/app/features/cart/model/cart-model.ts
@@ -1,4 +1,5 @@
 import { CartItemResponse, TotalPrice } from '../interfaces/cart-response';
+import { calculateDiscount } from '../../../shared/utils/calculate-discount';
 
 export class CartModel {
   constructor(
@@ -9,13 +10,40 @@ export class CartModel {
     public totalItemQuantity: number,
   ) {}
 
-  public printableTotalPrice(): string {
-    const result = this.totalPrice.centAmount / 100;
-
-    return result.toString();
+  public printableTotalPrice(): number {
+    return this.totalPrice.centAmount / 100;
   }
 
   public currencyCode(): string {
     return this.totalPrice.currencyCode;
+  }
+
+  public getShopDiscount(): number {
+    let discount: number = 0;
+
+    this.lineItems
+      .filter((lineItem: CartItemResponse) => lineItem.price.discounted)
+      .forEach((lineItem: CartItemResponse) => {
+        const originalPrice: number = lineItem.price.value.centAmount;
+        const discountedPrice: number = lineItem.price.discounted?.value.centAmount ?? 0;
+        const quantity: number = lineItem.quantity;
+
+        discount += calculateDiscount(originalPrice, discountedPrice, quantity);
+      });
+
+    return discount / 100;
+  }
+
+  public getItemsTotalPrice(): number {
+    let totalOriginalPrice = 0;
+
+    this.lineItems.forEach((lineItem) => {
+      const originalPrice: number = lineItem.price.value.centAmount;
+      const quantity: number = lineItem.quantity;
+
+      totalOriginalPrice += originalPrice * quantity;
+    });
+
+    return totalOriginalPrice / 100;
   }
 }
