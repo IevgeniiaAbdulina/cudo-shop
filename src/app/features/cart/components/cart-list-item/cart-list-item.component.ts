affaf110fb75c1ec7c10356bb4a63d4f6dfdb19f
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2025-06-15 16:09:55 +0200

    feat: Add Data Updating after changes the quantity of a product, add Total Cost Recalculation.

--- src/app/features/cart/components/cart-list-item/cart-list-item.component.ts
+++ src/app/features/cart/components/cart-list-item/cart-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, InputSignal, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, effect, input, InputSignal, output, OutputEmitterRef } from '@angular/core';
 import { ButtonComponent } from '../../../../shared/ui/button/button.component';
 import { DecimalPipe, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
@@ -12,54 +12,31 @@ import { discountValue } from '../../../../shared/utils/discount-value';
   templateUrl: './cart-list-item.component.html',
   styleUrl: './cart-list-item.component.scss',
 })
-export class CartListItemComponent implements OnInit {
+export class CartListItemComponent {
   protected readonly discountValue = discountValue;
-  public count: WritableSignal<number> = signal(1);
   public cartItem: InputSignal<CartItemResponse | undefined> = input<CartItemResponse>();
-  public item: WritableSignal<CartItemModel | undefined> = signal<CartItemModel | undefined>(undefined);
+  public itemModel: CartItemModel | null = null;
 
-  public ngOnInit(): void {
-    if (this.cartItem()) {
-      const itemResponse: CartItemResponse = this.cartItem()!;
+  public incrementItem: OutputEmitterRef<string | undefined> = output<string | undefined>();
+  public decrementItem: OutputEmitterRef<string | undefined> = output<string | undefined>();
 
-      console.log('[cart item] price ', itemResponse.price.discounted?.value.centAmount);
-      this.item.set(
-        new CartItemModel(
-          itemResponse.productId,
-          itemResponse.productKey,
-          itemResponse.name['en-US'],
-          itemResponse.variant.images[0].url,
-          itemResponse.price.value.currencyCode,
-          itemResponse.price.value.centAmount,
-          itemResponse.quantity,
-          itemResponse.totalPrice.centAmount,
-          itemResponse.price.discounted?.value.centAmount,
-        ),
-      );
+  constructor() {
+    effect(() => {
+      if (this.cartItem()) {
+        const item: CartItemResponse = this.cartItem()!;
 
-      this.count.set(itemResponse.quantity);
-    }
-  }
-
-  public removeCartItem(itemId: string | undefined) {
-    if (itemId) {
-      console.log('[cart list item] remove item id: ', itemId);
-    }
-  }
-
-  public increment(): void {
-    if (this.count() < 100) {
-      this.count.set(this.count() + 1);
-    }
-
-    return;
-  }
-
-  public decrement(key: string | undefined): void {
-    if (this.count() > 1) {
-      this.count.set(this.count() - 1);
-    } else {
-      this.removeCartItem(key);
-    }
+        this.itemModel = new CartItemModel(
+          item.productId,
+          item.productKey,
+          item.name['en-US'],
+          item.variant.images[0].url,
+          item.price.value.currencyCode,
+          item.price.value.centAmount,
+          item.quantity,
+          item.totalPrice.centAmount,
+          item.price.discounted?.value.centAmount,
+        );
+      }
+    });
   }
 }
