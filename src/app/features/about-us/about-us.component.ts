3fb60a2856f1ed41e84c544b444dbf2664b4e382
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2025-06-18 19:14:11 +0200

    feat: Add About-us page Technology section.

--- src/app/features/about-us/about-us.component.ts
+++ src/app/features/about-us/about-us.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss',
 })
-export class AboutUsComponent {}
+export class AboutUsComponent {
+  public technology = [
+    { name: 'Angular', icUrl: '../../assets/images/technology/Angular-Dark.svg' },
+    { name: 'TypeScript', icUrl: '../../assets/images/technology/TypeScript.svg' },
+    { name: 'Sass', icUrl: '../../assets/images/technology/Sass.svg' },
+    { name: 'Jest', icUrl: '../../assets/images/technology/Jest.svg' },
+    { name: 'Postman', icUrl: '../../assets/images/technology/Postman.svg' },
+    { name: 'Npm', icUrl: '../../assets/images/technology/Npm-Dark.svg' },
+    { name: 'Webpack', icUrl: '../../assets/images/technology/Webpack-Dark.svg' },
+    { name: 'Git', icUrl: '../../assets/images/technology/Git.svg' },
+    { name: 'GitHub', icUrl: '../../assets/images/technology/Github-Dark.svg' },
+    { name: 'VSCode', icUrl: '../../assets/images/technology/VSCode-Dark.svg' },
+    { name: 'WebStorm', icUrl: '../../assets/images/technology/WebStorm-Dark.svg' },
+    { name: 'Diagrams', icUrl: '../../assets/images/technology/Diagrams.svg' },
+  ];
+}
