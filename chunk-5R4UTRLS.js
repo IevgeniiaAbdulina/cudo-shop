811445f87fb5140d473660ec27fdbc1477296220
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2025-06-22 17:28:10 +0200

    Auto-generated commit

---
+++ chunk-5R4UTRLS.js
@@ -0,0 +1,1 @@
+import{X as o,h as r}from"./chunk-N4HGW3UR.js";var n=(()=>{class e{routeSource=new r;routeName$=this.routeSource.asObservable();setRoute(t){this.routeSource.next(t)}static \u0275fac=function(c){return new(c||e)};static \u0275prov=o({token:e,factory:e.\u0275fac,providedIn:"root"})}return e})();export{n as a};
--- chunk-HFFER3SE.js
+++
@@ -1,1 +0,0 @@
-var t=function(r){return r.CUSTOMERS="customers",r.PRODUCTS="products",r.PRODUCT_SEARCH="product-projections/search",r.CATEGORIES="categories",r}(t||{}),e=t;export{e as a};
